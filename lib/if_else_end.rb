598c217f728927b58f593499a12734b07f73bc25
generic-state-5961 <ericjchristine@gmail.com>
2020-07-31 21:27:27 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -7,3 +7,19 @@ if current_time % 2 == 0
 else
   puts "Odd!"
 end
+
+
+=begin 
+Also we can use the built in .even method...
+
+
+current_time = Time.now
+current_time = current_time.to_i
+ 
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
+
+=end
